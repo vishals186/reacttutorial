e95302aeb3365d0bdbd66243d8b026e28dd3097c
vishal.singh1 <vishal.singh1@appinventiv.com>
2023-10-19 13:23:33 +0530

    Init

---
+++ App.js
@@ -0,0 +1,82 @@
+{
+  /* <h1>React First Program</h1>; */
+}
+// const heading = React.createElement("h1", {}, "React First Program");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+{
+  /* <div id="parent">
+<div id="child">
+  <h1>Hello H1 Tag</h1>
+</div>
+</div> */
+}
+
+// const heading = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", {}, "Hello H1 Tag")
+//   )
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+// {
+/* <div id="parent">
+  <div id="child">
+    <h1>Hello H1 Tag</h1>
+    <h2>Hello H1 Tag</h2>
+  </div>
+  </div> */
+// }
+
+// const heading = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "Hello H1 Tag"),
+//     React.createElement("h2", {}, "Hello H2 Tag"),
+//     React.createElement("h3", {}, "Hello H3 Tag"),
+//   ])
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+// {
+/* <div id="parent">
+  <div id="child1">
+    <h1>Hello H1 Tag</h1>
+    <h2>Hello H1 Tag</h2>
+  </div>
+   <div id="child"2>
+    <h1>Hello H1 Tag</h1>
+    <h2>Hello H1 Tag</h2>
+  </div>
+  </div> */
+// }
+// const heading = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1" }, [
+//     React.createElement("h1", {}, "Hello child1 H1 Tag"),
+//     React.createElement("h2", {}, "Hello child1 H2 Tag"),
+//     React.createElement("h3", {}, "Hello child1 H3 Tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "Hello child2 H1 Tag"),
+//     React.createElement("h2", {}, "Hello child2 H2 Tag"),
+//     React.createElement("h3", {}, "Hello child2 H3 Tag"),
+//   ]),
+// ]);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+return (
+  <div>
+    <h1>HJEllo</h1>
+  </div>
+);
